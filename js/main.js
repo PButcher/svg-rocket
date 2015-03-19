07a614497eaa02e96a8bb490e1ea503ea9fa3f98
Peter Butcher <pbutcher93@gmail.com>
2015-03-19 14:11:00 +0000

    SVG.JS

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var canvasWidth = 550;
 var canvasHeight = 400;
 var showSplash = false;
 
-$(document).ready(function() {
+$(document).ready(function (){
 
 	// Remove welcome modal whilst testing
 	if(!showSplash) {
@@ -20,6 +20,9 @@ $(document).ready(function() {
 
 	// Set up workspace-canvas
 	setupWorkspaceCanvas();
+
+	// Set up toolkit
+	setupToolkit();
 });
 
 
@@ -50,23 +53,80 @@ function setupWelcomeModal() {
 // Set up workspace-canvas
 function setupWorkspaceCanvas() {
 	var paper = Raphael("ws-canvas", canvasWidth, canvasHeight);
-	paper.add([
-    {
-        type: "circle",
-        cx: 50,
-        cy: 50,
-        r: 50,
-        fill: "#FF0000"
-    },
-    {
-        type: "rect",
-        x: 50,
-        y: 50,
-        width: 50,
-        height: 50,
-        fill: "#fc0"
-    }
-	]);
+	var exampleSketch, exampleSketch2 = [
+	{
+		type: "circle",
+		cx: 50,
+		cy: 50,
+		r: 50,
+		fill: "#FF0000",
+		"stroke-width": 0,
+		stroke: "#000"
+	},
+	{
+		type: "rect",
+		x: 50,
+		y: 50,
+		width: 50,
+		height: 50,
+		fill: "#FF0000",
+		"stroke-width": 0,
+		stroke: "#000"
+	},
+	{
+		type: "rect",
+		x: 50,
+		y: 50,
+		width: 25,
+		height: 25,
+		fill: "#FFF",
+		"stroke-width": 0
+	},
+	{
+		type: "circle",
+		cx: 50,
+		cy: 50,
+		r: 25,
+		fill: "#FFF",
+		"stroke-width": 0
+	}
+	// {
+	// 	type: "text",
+	// 	x: 150,
+	// 	y: 100,
+	// 	text: "Some Text",
+	// 	fill: "#000"
+	// }
+	];
+
+	var r1 = paper.rect(100,100,200,10,5).attr({fill:'white'});
+	var r2 = paper.rect(50,200,100,15,5).attr({fill:'white'});
+	var r3 = paper.rect(200,100,200,10,5).attr({fill:'white'});
+	var r4 = paper.rect(150,200,100,15,5).attr({fill:'white'});
+
+	var shape = paper.set(r1, r2);
+	var shape2 = paper.set(r3, r4);
+
+	var l_coords = shape.getBBox().x,
+		r_coords = shape.getBBox().x2,
+		t_coords = shape.getBBox().y,
+		b_coords = shape.getBBox().y2;
+
+	var l2_coords = shape2.getBBox().x,
+		r2_coords = shape2.getBBox().x2,
+		t2_coords = shape2.getBBox().y,
+		b2_coords = shape2.getBBox().y2;
+
+	var cx = (l_coords + r_coords) / 2,
+		cy = (t_coords + b_coords) / 2;
+
+	var cx2 = (l_coords + r_coords) / 2,
+		cy2 = (t_coords + b_coords) / 2;
+
+	shape.rotate(45, cx, cy);
+	shape2.rotate(45, cx2, cy2);
+
+
 }
 
 // Set up event listeners
@@ -82,17 +142,26 @@ function setupEventListeners() {
 		pageTransition("output");
 	});
 
-	// Canvas width
+}
+
+// Set up toolkit
+function setupToolkit() {
+
+	// Set initial canvas width and height input values
+	$('#canvas-width').val(canvasWidth);
+	$('#canvas-height').val(canvasHeight);
+
+	// Canvas width input
 	$('#canvas-width').change(function() {
-		if(($('#canvas-width').val() > 0) && ($('#canvas-width').val() < 1000)) {
+		if(($('#canvas-width').val() > 0) && ($('#canvas-width').val() <= 1000)) {
 			canvasWidth = $('#canvas-width').val();
 			$('#ws-canvas').css("width", canvasWidth);
 		}
 	});
 
-	// Canvas height
+	// Canvas height input
 	$('#canvas-height').change(function() {
-		if(($('#canvas-height').val() > 0) && ($('#canvas-height').val() < 1000)) {
+		if(($('#canvas-height').val() > 0) && ($('#canvas-height').val() <= 1000)) {
 			canvasHeight = $('#canvas-height').val();
 			$('#ws-canvas').css("height", canvasHeight);
 		}
